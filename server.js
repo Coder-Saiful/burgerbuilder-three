cfc5d74e6efd533a34674e5a27e092dd2ced6803
Coder Saiful <md.syful.max970@gmail.com>
2022-10-03 06:54:15 -0700

    Changed server.js fike

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config();
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_SERVER).then(() => console.log('Connected to MongoDB Successfully!')).catch(err => console.log('MongoDB Connection Failed!'));
+mongoose.connect(process.env.MONGODB_SERVER).then(() => console.log('Connected to MongoDB Successfully...')).catch(err => console.log('MongoDB Connection Failed!'));
 
 const port = process.env.PORT || 3001;
 
